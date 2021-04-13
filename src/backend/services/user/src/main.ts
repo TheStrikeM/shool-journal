ac9eb3c797459c6aae83d2826042d05e0483ed11
TheStrikeM <dinkodanil@gmail.com>
2021-04-13 20:07:15 +0500

    nest new in all backend services and start developing user service

---
+++ src/backend/services/user/src/main.ts
@@ -0,0 +1,27 @@
+import {NestFactory} from "@nestjs/core";
+import {MicroserviceOptions, Transport} from "@nestjs/microservices";
+import {AppModule} from "./app.module";
+import {join} from "path";
+import {Logger} from "@nestjs/common";
+
+const logger = new Logger('Main')
+async function start() {
+    try {
+        const PORT = process.env.PORT || 5000,
+            app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+                transport: Transport.GRPC,
+                options: {
+                    package: 'user',
+                    protoPath: join(__dirname, '../proto/user.proto'),
+                }
+            })
+
+        app.listen(
+            () => logger.log(`Server has started on port https://localhost:${PORT}/`)
+        )
+    } catch(e) {
+        logger.error(e.message)
+    }
+}
+
+start()
